c6aad95ed68364dad6213f8e879177859c7b982a
Adam Thompson <adam@serialphotog.com>
2016-07-05 14:58:36 -0400

    Installer now downloads to /tmp

--- scripts/install_omzsh.sh
+++ scripts/install_omzsh.sh
@@ -2,8 +2,8 @@
 
 # Install Oh-My-ZSH
 if [ -d '${HOME}/.oh-my-zsh' ]; then
-	curl -L http://install.ohmyz.sh > install.sh
-	sh install.sh
+	curl -L http://install.ohmyz.sh > /tmp/install.sh
+	sh /tmp/install.sh
 
 	# Set ZSH as primary shell
 	sudo chsh -s $(which zsh) $(whoami)
