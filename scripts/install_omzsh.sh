6fd23ea5af6112b78c3f9ddf189faae9299e4b4c
Adam Thompson <adam@serialphotog.com>
2016-07-05 12:55:46 -0400

    install only if needed

--- scripts/install_omzsh.sh
+++ scripts/install_omzsh.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Install Oh-My-ZSH
-curl -L http://install.ohmyz.sh > install.sh
-sh install.sh
+if [! -d ~/.oh-my-zsh]; then
+	curl -L http://install.ohmyz.sh > install.sh
+	sh install.sh
 
-# Set ZSH as primary shell
-sudo chsh -s $(which zsh) $(whoami)
+	# Set ZSH as primary shell
+	sudo chsh -s $(which zsh) $(whoami)
+fi
